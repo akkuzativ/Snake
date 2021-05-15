a728166d7afacb4f36bbe0019145d133b7d781b9
KarolinaGl <karolina.gluszek6@gmail.com>
2021-05-15 19:44:48 +0200

    Add BoardGenerator class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,26 +2,13 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-        Board board = new Board(25, 25);
-        Fruit fruit = new Fruit(new Coordinates(20, 20));
-        Frog frog = new Frog(new Coordinates(10, 10));
-        Snake enemySnake = new Snake(new ArrayList<>(), new Coordinates(20, 12), Direction.UP);
-        Snake snake = new Snake(new ArrayList<>(), new Coordinates(10, 12), Direction.UP);
-        try {
-            board.addFruit(fruit);
-            board.setSnake(snake);
-            board.setFrog(frog);
-            board.setEnemySnake(enemySnake);
-        }
-        catch (TileOutOfBoundsException e) {
-            System.out.println("Out of boards bounds");
-            System.exit(-1);
-        }
+        BoardGenerator boardGenerator = new BoardGenerator(20, 20, 5, 10, 1);
+        Board board = boardGenerator.generateBoard();
+
         JFrame gameFrame = new JFrame();
         gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         gameFrame.setSize(new Dimension(800, 800));
